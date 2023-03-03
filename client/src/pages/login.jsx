08838934c368c40d5a778679bd19f1543a9ceb45
Phan Vuong <pdvongtb@gmail.com>
2023-03-03 11:40:47 +0700

    login

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { request } from "../utils/fetchApi";
+import { login } from "../redux/authSlice";
+import { useDispatch } from "react-redux";
 
-const login = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (email === "" || password === "") return;
+
+    try {
+      const options = {
+        "Content-Type": "application/json",
+      };
+
+      const data = await request("/auth/login", "POST", options, {
+        email,
+        password,
+      });
+      console.log(data);
+      dispatch(login(data));
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div>login</div>
-  )
-}
+    <div>
+      <div className="login-container">
+        <div className="login-wrapper">
+          <h2>Login</h2>
+          <form className="login-form" onSubmit={handleLogin}>
+            <input
+              type="email"
+              placeholder="Email..."
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password..."
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+            <p>
+              Don't have an account? <Link to="/register">Register</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default login
+export default Login;
